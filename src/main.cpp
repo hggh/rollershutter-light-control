d39503b14d510a23930363614acd3e829afb6321
Jonas Genannt <jonas@brachium-system.net>
2019-03-08 23:18:55 +0100

    added pio

---
+++ src/main.cpp
@@ -0,0 +1,110 @@
+#include "Arduino.h"
+#include <ShiftOutRegister.h>
+#include <Shutter.h>
+#include <Light.h>
+#include <ESP8266WiFi.h>
+
+extern "C" {
+#include "user_interface.h"
+}
+
+#include "config.h"
+
+WiFiServer server(80);
+ShiftOutRegister sh_register;
+Shutter shutter1;
+Light light1;
+Light light2;
+
+os_timer_t Timer1;
+
+void timerCall(void*z) {
+  shutter1.timer();
+}
+
+void setup_timer1(void) {
+  os_timer_setfn(&Timer1, timerCall, NULL);
+  os_timer_arm(&Timer1, 1000, true);
+}
+
+void wificonnect() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(SECRET_SSID, SECRET_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(200);
+  }
+}
+
+void setup() {
+  setup_timer1();
+  wificonnect();
+
+  server.begin();
+
+  /**
+     D1 == data
+     D2 == shift
+     D5 == store
+     D6 == enable/OE
+  **/
+  sh_register.init(D1, D2, D5, D6, 8);
+
+  // light 1 == K2
+  light1.begin(0, &sh_register);
+  // light 2 == K3
+  light2.begin(1, &sh_register);
+  // 2 = power ; 3 = UP/DOWN
+  shutter1.begin(2, 3, 20, &sh_register);
+
+}
+
+void loop() {
+  if (WiFi.status() != WL_CONNECTED) {
+    wificonnect();
+  }
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+
+  // Wait until the client sends some data
+  while (!client.available()) {
+    delay(1);
+  }
+
+  // Read the first line of the request
+  String header_line = client.readStringUntil('\r');
+  client.flush();
+
+  // Light 1
+  if (header_line.startsWith("GET /light1/on")) {
+    light1.on();
+  }
+  if (header_line.startsWith("GET /light1/off")) {
+    light1.off();
+  }
+
+  // Light 2
+  if (header_line.startsWith("GET /light2/on")) {
+    light2.on();
+  }
+  if (header_line.startsWith("GET /light2/off")) {
+    light2.off();
+  }
+
+  // Shutter1
+  if (header_line.startsWith("GET /shutter1/down")) {
+    shutter1.down();
+  }
+  if (header_line.startsWith("GET /shutter1/up")) {
+    shutter1.up();
+  }
+  if (header_line.startsWith("GET /shutter1/halt")) {
+    shutter1.halt();
+  }
+
+  String s = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nOK";
+  client.print(s);
+  delay(1);
+}
